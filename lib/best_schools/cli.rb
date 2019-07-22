4963e027a79ea918413f4d33794d4e89d998713c
Tam Vu <vutam_a@hotmail.com>
2019-07-22 22:30:52 +0000

    added beginning cli methods

--- lib/best_schools/cli.rb
+++ lib/best_schools/cli.rb
@@ -3,8 +3,23 @@
 class BestSchools::CLI 
   
   def call
-    puts "Welcome to Niche. The best districts in the Houston Area of 2019 are:"
-    binding.pry 
+    puts "Welcome! According to Niche, the best districts in 2019 of the Houston Area are:"
+    get_district_rankings
+    #list_districts 
+    get_user_district_selection
+    #list_district_info
   end 
     
+  def get_district_rankings
+    #scrape the districts off the site, put it in array?...remember to try to take out sponsored districts
+    @districts = ["Friendswood", "Katy", "Tomball", "Pearland"]
+  end 
+    
+  def get_user_district_selection
+    #list the districts
+    @districts.each_with_index{|district, index|
+      puts "#{index.to_i + 1}. #{district}"
+    } 
+  end 
+  
 end 
