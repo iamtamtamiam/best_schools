087576178a3e8d6961780d89cdb97c3b117026fa
Tam Vu <vutam_a@hotmail.com>
2019-07-23 22:57:30 +0000

    add scrape site

--- lib/best_schools/scraper.rb
+++ lib/best_schools/scraper.rb
@@ -1,7 +1,7 @@
 class BestSchools::Scraper
   
+  doc = Nokogiri::HTML(open("https://www.niche.com/k12/search/best-school-districts/m/houston-metro-area/"))
   
-  
-  
+  districts = page.css("div.search-results ol.search-results__list li.search-results__list__item div.search-result div.card a.search-result__link div.card__inner h2")
   
 end 
